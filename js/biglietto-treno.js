29c2018be733cfd2c50cea4ad16361b99f4602e1
FrensysThomas87 <delta873@gmail.com>
2020-12-14 16:13:41 +0100

    ultime modifiche

--- js/biglietto-treno.js
+++ js/biglietto-treno.js
@@ -10,12 +10,18 @@ console.log('Il tizio ha ' + age + ' anni');
 
 var kilometersRate = 0.21;
 
-var totalPrice = age * kilometersRate;
+var totalPrice = kilometers * kilometersRate;
 
-console.log('il prezzo totale è ' + totalPrice);
+
 
 if(isNaN(kilometers) || isNaN(age)){
   alert('L\'età e i kilometri devono essere un numero');
 }else if(age < 18){
-  totalPrice = totalPrice - (totalPrice * (20 / 100));
+  totalPrice = (totalPrice * 20) / 100;
+  alert('Il prezzo da pagare è ' + totalPrice);
+}else if(age >= 65){
+  totalPrice = (totalPrice * 40) / 100;
+  alert('Il prezzo da pagare è ' + totalPrice);
+}else{
+  alert('Il prezzo da pagare è ' + totalPrice);
 }
