4d0697c54c458e4020e09beebb255180bcb1b7ec
FrensysThomas87 <delta873@gmail.com>
2020-12-14 17:31:55 +0100

    aggiunta generazione randomica del prezzo di partenza

--- bonus/js/js-bonus.js
+++ bonus/js/js-bonus.js
@@ -2,11 +2,20 @@ var priceContainer = document.getElementById('box');
 
 var casual = Math.floor(Math.random() * 6) + 1;
 
+var startingPrice = Math.floor(Math.random() * 101);
+
+alert('Il prezzo di partenza è ' + startingPrice);
+
 var numeroScelto = parseInt(prompt('Quale numero uscirà ? Scegli un numero da 1 a 6, se il numero scelto corrisponde con il numero uscito nei dadi otterrai uno sconto del 50%!!!'));
+
+
+
+
+priceContainer.innerText = 'Hai digitato, ' + numeroScelto;
 
 console.log(casual);
 
-var startingPrice = 100;
+
 
 // console.log(typeof startingPrice);
 
@@ -14,9 +23,9 @@ if(isNaN(numeroScelto)){
   alert('Devi inserire un numero!!!!');
 }else if(casual!== numeroScelto){
   alert('Mi dispiace hai perso niente sconto!!')
-  priceContainer.innerText = 'Mi dispiace hai perso niente sconto!!'
+  priceContainer.append (' Mi dispiace hai perso niente sconto!!');
 }else{
-  startingPrice -= (100 / 100) * 50;
+  startingPrice -= (startingPrice / 100) * 50;
   alert('Complimenti hai vinto hai ottenuto lo sconto del 50%!!!! il prezzo da pagare è ' + startingPrice);
-  priceContainer.innerText = 'Complimenti hai vinto hai ottenuto lo sconto del 50%!!!!, il prezzo da pagare è ' + startingPrice;
+  priceContainer.append (' Complimenti hai vinto hai ottenuto lo sconto del 50%!!!!, il prezzo da pagare è ' + startingPrice);
 }
