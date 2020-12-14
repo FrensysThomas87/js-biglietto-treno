fdc75bf006450cbc0bd7f301277486f183b6dc8f
FrensysThomas87 <delta873@gmail.com>
2020-12-14 16:25:52 +0100

    corretto il messaggio su box in caso di mancato inserimento di un numero

--- js/biglietto-treno.js
+++ js/biglietto-treno.js
@@ -16,14 +16,16 @@ var totalPrice = kilometers * kilometersRate;
 
 if(isNaN(kilometers) || isNaN(age)){
   alert('L\'età e i kilometri devono essere un numero');
+  priceContainer.innerText = 'Devi inserire un numero sia in kilometri che in età';
 }else if(age < 18){
   totalPrice = totalPrice - (totalPrice / 100) * 20;
   alert('Il prezzo da pagare è ' + totalPrice);
+  priceContainer.innerText = 'Il prezzo da pagare è ' + totalPrice;
 }else if(age >= 65){
   totalPrice = totalPrice - (totalPrice / 100) * 40;
   alert('Il prezzo da pagare è ' + totalPrice);
+  priceContainer.innerText = 'Il prezzo da pagare è ' + totalPrice;
 }else{
   alert('Il prezzo da pagare è ' + totalPrice);
+  priceContainer.innerText = 'Il prezzo da pagare è ' + totalPrice;
 }
-
-priceContainer.innerText = 'Il prezzo da pagare è ' + totalPrice;
